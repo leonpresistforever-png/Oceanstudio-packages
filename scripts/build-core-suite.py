2e0b4fd29d41b9850c9c33bfadde396957919f4a
Leon <leonpresistforever@gmail.com>
2026-09-19 12:14:19 +0530

    Disable BusyBox UTMP WTMP features unavailable on Android Bionic

--- scripts/build-core-suite.py
+++ scripts/build-core-suite.py
@@ -120,7 +120,7 @@ def busybox(item,ndk,work,pool,readelf):
       "TC","HOSTID","CHVT","DEALLOCVT","DUMPKMAP","FGCONSOLE","KBD_MODE",
       "LOADFONT","LOADKMAP","OPENVT","RESET","RESIZE","SETCONSOLE",
       "SETFONT","SETKEYCODES","SHOWKEY",
-      "HALT","REBOOT","POWEROFF"
+      "HALT","REBOOT","POWEROFF","FEATURE_UTMP","FEATURE_WTMP"
     ]
     for symbol in android_off:
         text=re.sub(rf"^CONFIG_{symbol}=y$",f"# CONFIG_{symbol} is not set",text,flags=re.M)
